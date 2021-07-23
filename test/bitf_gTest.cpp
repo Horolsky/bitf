b803f5ecf06b49fae9707de48acedcc4eacd2fa5
Oleksandr Khorolskyi <horolsky89@outlook.com>
2021-07-23 21:06:11 +0300

    test: suites split

--- test/bitf_gTest.cpp
+++ test/bitf_gTest.cpp
@@ -33,48 +33,42 @@ public virtual mutator<size_t>
     using data<size_t>::bitsize;
 };
 
-TEST(BitF, bitsize)
+TEST(data_static, bitsize)
 {
 	EXPECT_EQ(BitF::bitsize(), sizeof(size_t) << 0b11);
 }
 
-TEST(BitF, nofbits)
+TEST(data_static, nofbits)
 {
 	EXPECT_EQ(BitF::nofbits(0b10101), 5);
 }
 
-
-TEST(BitF, init_def)
+TEST(data, init_def)
 {
     BitF x {};
     EXPECT_EQ(x.bits(), 0b0);
 }
-TEST(BitF, init_zero)
+
+TEST(data, init_zero)
 {
     BitF x {0};
     EXPECT_EQ(x.bits(), 0b0);
 }
-TEST(BitF, init_max)
+
+TEST(data, init_max)
 {
     BitF x {BitF::maxvalue()};
     EXPECT_EQ(x.bits(), BitF::maxvalue());
 }
 
-TEST(bitdata, set_zero)
-{
-    BitF x {243387867};
-    x.set(0);
-	EXPECT_EQ(x.bits(), 0b0);
-}
-
-TEST(bitdata, copy_constructor)
+TEST(data, copy_constructor)
 {
     BitF a {474434687};
     BitF b = a;
 	EXPECT_EQ(a.bits(), b.bits());
 }
 
-TEST(bitdata, assignment)
+TEST(data, assignment)
 {
     BitF a {474434687};
     BitF b {0};
@@ -82,49 +76,54 @@ TEST(bitdata, assignment)
 	EXPECT_EQ(a.bits(), b.bits());
 }
 
-TEST(bitdata, insert_1bit)
+TEST(mutator, set_zero)
+{
+    BitF x {243387867};
+    x.set(0);
+	EXPECT_EQ(x.bits(), 0b0);
+}
+
+TEST(mutator, insert_1bit)
 {
     BitF x {0b10001};
     x.insert(1,2,1);
 	EXPECT_EQ(x.bits(), 0b10101);
 }
 
-TEST(bitdata, isert_3bits)
+TEST(mutator, insert_3bits)
 {
     BitF x {0b10000001};
     x.insert(0b111,3,3);
 	EXPECT_EQ(x.bits(), 0b10111001);
 }
 
-TEST(bitdata, get_onebit)
+TEST(accessor, get_onebit)
 {
     BitF x {0b10101};
     size_t a = x.get(2, 1);
 	EXPECT_EQ(a, 0b1);
 }
 
-TEST(bitdata, get_3bits)
+TEST(accessor, get_3bits)
 {
     BitF x {0b10101};
     size_t a = x.get(2, 3);
 	EXPECT_EQ(a, 0b101);
 }
 
-TEST(bitdata, set_vector)
+TEST(mutator, set_vector)
 {
     BitF x {0};
     x.insert({1,0,2,0,3},1,2);
     auto bv = x.bits();
 	EXPECT_EQ(bv, 0b11001000010);
 }
-
-TEST(bitdata, get_vector)
+TEST(accessor, get_vector)
 {
-    BitF x {};
-    std::vector<size_t> vec {1,2,3,1,2,3};
-    x.insert(vec, 5, 2);
+    BitF x {0b1100100001};
+    std::vector<size_t> vec {1,0,2,0,3};
     bool equal = true;
-    auto ret_vec = x.get(5,2,vec.size());
+    auto ret_vec = x.get(0,2,vec.size());
      
     for (size_t i = 0; i < vec.size(); i++)
         if (vec[i] != ret_vec[i]) equal = false;
